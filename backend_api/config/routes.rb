32ae7774f76b01d7ef6d63b6bc2646a5751fe3a1
Nroulston <nicolas.roulston@gmail.com>
2020-10-14 13:05:17 -0700

    Add session controller logic  and routes

--- backend_api/config/routes.rb
+++ backend_api/config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:create, :show, :index]
+
+  post '/login',    to: 'sessions#create'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+  post '/logout',   to: 'sessions#destroy'
 end
