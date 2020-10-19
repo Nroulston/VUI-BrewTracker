5e0c565b9f1f38768cf6306d867a080c99132bb6
Nroulston <nicolas.roulston@gmail.com>
2020-10-19 12:04:13 -0700

    add strong paramaters for nested yeast and adjunct attributes

--- backend_api/app/controllers/recipes_controller.rb
+++ backend_api/app/controllers/recipes_controller.rb
@@ -19,7 +19,7 @@ class RecipesController < ApplicationController
   end
   private
   def recipe_params
-    params.require(:recipe).permit(:user_id, :name, :method, :boil_time, :batch_size, :pre_boil_size, :pre_boil_gravity, :target_fg, :target_og, :ibu, :srm, :mash_ph, :mash_schedule, :style, hops_attributes: [:name, :form, :alpha_acid], fermentables_attributes: [:name])
+    params.require(:recipe).permit(:user_id, :name, :method, :boil_time, :batch_size, :pre_boil_size, :pre_boil_gravity, :target_fg, :target_og, :ibu, :srm, :mash_ph, :mash_schedule, :style, hops_attributes: [:name, :form, :alpha_acid], fermentables_attributes: [:name], yeasts_attributes: [:name], other_ingredients_attributes: [:name])
   end
  
 
