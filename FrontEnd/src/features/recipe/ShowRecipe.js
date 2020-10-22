63423d77c63dbb11bb91da1b0d1af32ab1d1d84d
Nroulston <nicolas.roulston@gmail.com>
2020-10-22 10:43:30 -0700

    add MUI theme and format to ShowRecipe, clean up code

--- FrontEnd/src/features/recipe/ShowRecipe.js
+++ FrontEnd/src/features/recipe/ShowRecipe.js
@@ -1,21 +1,94 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
+import React from 'react';
+import { useSelector } from 'react-redux'
 import {selectRecipeById} from './recipesSlice'
-import axios from 'axios'
+
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import MenuIcon from '@material-ui/icons/Menu';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 import RecipeStatsShow from './ingredients/RecipeStatsShow'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+ 
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: 'none',
+  },
+  title: {
+    flexGrow: 1,
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+ 
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 240,
+  },
+}));
 
 
 export const ShowRecipe = ({match}) => {
   const { recipeId } = match.params
-  
+  const classes = useStyles();
   const recipe = useSelector( state => selectRecipeById(state, recipeId))
-  
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
   if(!recipe) {
     return (
       <section>
-
+        
 
         <h2>Post not found!</h2>
       </section>
@@ -23,13 +96,53 @@ export const ShowRecipe = ({match}) => {
   }
 
   return(
-    <section>
-      <article>
-        <h2>{recipe.name}</h2>
-        <RecipeStatsShow recipe={recipe} />
-      </article>
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="absolute" className={clsx(classes.appBar)}>
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            className={clsx(classes.menuButton)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+            Dashboard
+          </Typography>
+          <IconButton color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid container spacing={3}>
       
-    </section>
-    
+            <Grid item xs={12} >
+              <Paper className={fixedHeightPaper}>
+                <RecipeStatsShow recipe={recipe} />
+              </Paper>
+            </Grid>
+            
+            <Grid item xs={12} md={6} lg={3}>
+              <Paper className={fixedHeightPaper}>
+                {/* <RecipeGrainsShow */}
+              </Paper>
+            </Grid>
+            
+            <Grid item xs={12} md={6} lg={3}>
+              <Paper className={classes.paper}>
+                {/* <RecipeHopsShow /> */}
+              </Paper>
+            </Grid>
+          </Grid>
+        </Container>
+      </main>
+    </div>
   )
 }
